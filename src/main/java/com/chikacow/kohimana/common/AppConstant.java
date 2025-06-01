782d0e613cdc1eacd600c8edefc8e48098608887
chikacoww <caochikien2004@gmail.com>
2025-06-01 18:48:14 +0700

    version 3.1: ensured thread safe

--- src/main/java/com/chikacow/kohimana/common/AppConstant.java
+++ src/main/java/com/chikacow/kohimana/common/AppConstant.java
@@ -2,17 +2,25 @@ package com.chikacow.kohimana.common;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class AppConstant {
-    private static int REQUEST_COUNT = 0;
+    private static final AtomicInteger REQUEST_COUNT = new AtomicInteger(0);
 
     public static int increase() {
-        return ++REQUEST_COUNT;
+        return REQUEST_COUNT.incrementAndGet();
     }
+
     public static int getRequestCount() {
-        return REQUEST_COUNT;
+        return REQUEST_COUNT.get();
     }
 
     public static void reset() {
-        REQUEST_COUNT = 0;
+        REQUEST_COUNT.set(0);
+    }
+
+    public static int decrease() {
+        return REQUEST_COUNT.decrementAndGet();
     }
 }
+
